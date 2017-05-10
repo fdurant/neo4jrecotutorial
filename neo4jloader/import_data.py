ca315023f93d78dc71051e83fb4c7168c503cf82
fdurant <frederik.durant@pandora.be>
2017-05-10 12:16:35 +0100

    First working version that loads and shows groups

--- neo4jloader/import_data.py
+++ neo4jloader/import_data.py
@@ -4,29 +4,57 @@ from time import sleep
 import sys
 import os
 
-secondsToSleep = 10
+global session
 
-for i in range(5,0,-1):
+def initialize():
+
+    global session
+    global driver
+
+    secondsToSleep = 10
+
+    for i in range(5,0,-1):
+        try:
+            driver = GraphDatabase.driver("bolt://neo4j:7687", auth=basic_auth(os.environ['NEO4J_AUTH'].split('/')[0], "12345"))
+            session = driver.session()
+        except (ServiceUnavailable, ClientError, AuthError, SecurityError) as e:
+            if i == 1:
+                raise
+            sleep(secondsToSleep)
+            print('retry %d after catching %s; sleeping for %d seconds' % (i,str(e), secondsToSleep))
+        else:
+            break
+
+def empty_db():
+
     try:
-        driver = GraphDatabase.driver("bolt://neo4j:7687", auth=basic_auth(os.environ['NEO4J_AUTH'].split('/')[0], "12345"))
-        session = driver.session()
-    except (ServiceUnavailable, ClientError, AuthError, SecurityError) as e:
-        if i == 1:
-            raise
-        sleep(secondsToSleep)
-        print('retry %d after catching %s; sleeping for %d seconds' % (i,str(e), secondsToSleep))
-    else:
-        break    
+        result = session.run('MATCH (n) RETURN (n) LIMIT 1')
+        res = result.peek()
+        # Something found, so delete all nodes
+        result = session.run('MATCH (n) DETACH DELETE(n)')
+        print >> sys.stderr, "Emptying Neo4J database"
+    except:
+        print >> sys.stderr, "Neo4J database is already empty, so no need to empty it again"
 
-cypher = "LOAD CSV WITH HEADERS"
-cypher += 'FROM "file://groups.csv"'        
-cypher += "AS row"
-cypher += "RETURN row"
-cypher += "CREATE (:Group { id:row, name:row, urlname:row.urlname, rating:toInt(row.rating), created:toInt(row.created))"
-result = session.run(cypher)
-result = session.run("MATCH (g:Group) RETURN g.id, g.name, g.urlname LIMIT 5")
+def load_groups():
 
-for record in result:
-    print >> sys.stderr, record
+    cypher = "LOAD CSV WITH HEADERS"
+    cypher += ' FROM "file:///groups.csv"'
+    cypher += " AS row"
+    cypher += " CREATE (:Group { id:row.id, name:row.name, urlname:row.urlname, rating:toInt(row.rating), created:toInt(row.created) })"
+    result = session.run(cypher)
+    for record in result:
+        print >> sys.stderr, record
 
-#cypher = "MERGE (node:Label )"
+def show_groups():
+
+    result = session.run("MATCH (g:Group) RETURN g.id, g.name, g.urlname LIMIT 5")
+
+    for record in result:
+        print >> sys.stderr, record
+
+if __name__ == "__main__":
+    initialize()
+    empty_db()
+    load_groups()
+    show_groups()
